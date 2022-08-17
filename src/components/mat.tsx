b71403442ed97099094ac6c5e20d9c6d4865dd46
Monish <monishkumar833@gmail.com>
2022-08-17 10:26:31 -0400

    can now have a deck and add cards to hand

--- src/components/mat.tsx
+++ src/components/mat.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './mat.css';
 
 function Mat() {
+    let id: string = "";
+    let count: number = 0;
+    const [hand, setHand] = useState([{ src: "", value: "", suit: "" }]);
+
+    function getData() {
+        fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+            .then((response) => response.json())
+            .then((data) => {
+                assignDeck(data)
+            })
+    }
+
+    function assignDeck(data: any) {
+        id = data.deck_id;
+        count = data.remaining as unknown as number;
+    }
+
+    function draw() {
+        fetch("https://www.deckofcardsapi.com/api/deck/" + id + "/draw/?count=1")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                assignHand(data)
+            })
+    }
+
+    function assignHand(data: any) {
+        setHand(hand => [...hand, { src: data.cards[0].image, value: data.cards[0].value, suit: data.cards[0].suit }]);
+    }
+
     return (
         <div className="mat">
-
+            <div>
+                <button onClick={getData}>start</button>
+            </div>
+            <div>
+                <button onClick={draw}>draw</button>
+            </div>
+            <div>
+                {hand.map((card) => {
+                    return (
+                        <div className="item">
+                            <img src={card.src} alt="" />
+                            {card.value}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
