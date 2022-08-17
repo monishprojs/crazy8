41c63f9222d2491edccb49dc3d7df0efac63aeb7
Monish <monishkumar833@gmail.com>
2022-08-17 09:58:15 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Mat from './components/mat';
+
+function App() {
+  return (
+    <Mat></Mat>
+  );
+}
+
+export default App;
---
+++ src/components/mat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './mat.css';
+
+function Mat() {
+    return (
+        <div className="mat">
+
+        </div>
+    );
+}
+
+export default Mat;
