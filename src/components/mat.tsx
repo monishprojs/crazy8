cec54e0d8f13feabb52ee5503fb68823f4adda69
Monish <monishkumar833@gmail.com>
2022-08-17 11:46:28 -0400

    added the three other computer players

--- src/components/mat.tsx
+++ src/components/mat.tsx
@@ -4,9 +4,14 @@ import './mat.css';
 function Mat() {
     let id: string = "jtatq55nbryd";
     let count: number = 0;
+    let turn: number = 0;
     const crazy: string[] = ["ace", "jack", "king", "queen"]
     const [hand, setHand] = useState([{ src: "", value: "", suit: "" }]);
-    const [pile, setPile] = useState([{ src: "", value: "", suit: "" }])
+    const [hand1, setHand1] = useState([{ src: "", value: "", suit: "" }]);
+    const [hand2, setHand2] = useState([{ src: "", value: "", suit: "" }]);
+    const [hand3, setHand3] = useState([{ src: "", value: "", suit: "" }]);
+    const [pile, setPile] = useState([{ src: "", value: "", suit: "" }]);
+
 
     function getData() {
         fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
@@ -17,13 +22,14 @@ function Mat() {
             })
     }
 
-    function draw() {
-        fetch("https://www.deckofcardsapi.com/api/deck/" + id + "/draw/?count=5")
+    function drawStart() {
+        fetch("https://www.deckofcardsapi.com/api/deck/" + id + "/draw/?count=20")
             .then((response) => response.json())
             .then((data) => {
                 console.log(data)
-                assignHand(data)
+                assignStartingHand(data)
                 assignDeck(data)
+                console.log(data)
             })
     }
 
@@ -32,9 +38,18 @@ function Mat() {
     }
 
 
-    function assignHand(data: any) {
+    function assignStartingHand(data: any) {
         for (let i = 0; i < 5; ++i) {
             setHand(hand => [...hand, { src: data.cards[i].image, value: data.cards[i].value, suit: data.cards[i].suit }]);
+        }
+        for (let i = 5; i < 10; ++i) {
+            setHand1(hand1 => [...hand1, { src: data.cards[i].image, value: data.cards[i].value, suit: data.cards[i].suit }]);
+        }
+        for (let i = 10; i < 15; ++i) {
+            setHand2(hand2 => [...hand2, { src: data.cards[i].image, value: data.cards[i].value, suit: data.cards[i].suit }]);
+        }
+        for (let i = 15; i < 20; ++i) {
+            setHand3(hand3 => [...hand3, { src: data.cards[i].image, value: data.cards[i].value, suit: data.cards[i].suit }]);
         }
     }
 
@@ -73,11 +88,11 @@ function Mat() {
     }, [])
     return (
         <div className="mat">
-            {/* <div>
-                <button onClick={getData}>start</button>
-            </div> */}
             <div>
-                <button onClick={draw}>draw</button>
+                <button onClick={() => drawStart()}>start</button>
+            </div>
+            <div>
+                <button >draw</button>
             </div>
             <div className='items'>
                 {hand.map((card, index) => {
@@ -88,7 +103,34 @@ function Mat() {
                     );
                 })}
             </div>
-            <div>
+            <div className='items'>
+                {hand1.map((card, index) => {
+                    return (
+                        <div className="item">
+                            <img className='card' onClick={() => addPile(index)} src={card.src} alt="" />
+                        </div>
+                    );
+                })}
+            </div>
+            <div className='items'>
+                {hand2.map((card, index) => {
+                    return (
+                        <div className="item">
+                            <img className='card' onClick={() => addPile(index)} src={card.src} alt="" />
+                        </div>
+                    );
+                })}
+            </div>
+            <div className='items'>
+                {hand3.map((card, index) => {
+                    return (
+                        <div className="item">
+                            <img className='card' onClick={() => addPile(index)} src={card.src} alt="" />
+                        </div>
+                    );
+                })}
+            </div>
+            <div className='pile'>
                 {pile.map((card) => {
                     return (
                         <div className="item">
